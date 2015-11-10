d927b99b168aedeed9c07129965541407b435133
Jonathon <jonathon.nordquist@gmail.com>
2015-11-10 22:07:38 +0000

    Add sessions helper to application controller.  Create log_in method in sessions helper.  Add log_in call to sessions create method.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,10 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:sessions][:email].downcase)
     if user && user.authenticate(params[:sessions][:password])
-      p "it worked================================"
+      log_in user
+      redirect_to user
     else
-      flash[:danger] = "Email and password combination is incorrect"
+      flash.now[:danger] = "Email and password combination is incorrect"
       render 'new'
     end
   end
