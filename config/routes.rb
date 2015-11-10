fc03cd689a16d7f3a3cdcb1313f49608c8fa892c
Jonathon <jonathon.nordquist@gmail.com>
2015-11-10 15:33:47 +0000

    Create static pages controller.  Add views, set routes to views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root                     'static#home'
+
+  get 'about'           => 'static#about'
+
+  get 'help'            => 'static#help'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
