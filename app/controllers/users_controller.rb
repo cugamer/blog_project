03e3232b663a87e0008f1adfef1ee5ad15b6d235
Jonathon <jonathon.nordquist@gmail.com>
2015-11-10 17:37:15 +0000

    Add validations to user model.  Add user_params and create methods to users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    user.save
+    
+    redirect_to root_url
+  end
+  
+  private
+    def user_params
+      params.require(:users).permit(:user_name, :name, :email)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  validates :user_name, presence: true
+  validates :name, presence: true
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
