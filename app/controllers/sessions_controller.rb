b4fbda767add2cd16066d5567e513b8969f7fa42
Jonathon <jonathon.nordquist@gmail.com>
2015-11-10 22:02:19 +0000

    Add find_by to sessions create to find user, conditional to determine what to do if user does or does not authenticate.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,12 @@ class SessionsController < ApplicationController
   end
   
   def create
+    user = User.find_by(email: params[:sessions][:email].downcase)
+    if user && user.authenticate(params[:sessions][:password])
+      p "it worked================================"
+    else
+      flash[:danger] = "Email and password combination is incorrect"
+      render 'new'
+    end
   end
 end
