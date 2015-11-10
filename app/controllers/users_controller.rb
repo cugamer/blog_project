541ff9992a2302e086663be44ca947dec68284a3
Jonathon <jonathon.nordquist@gmail.com>
2015-11-10 18:07:34 +0000

    Create _error_messages partial.  Add new method to users controller.  Add handling of successful and unsuccessful user create to controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,15 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
   def create
-    user = User.new(user_params)
-    user.save
-    
-    redirect_to root_url
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
   
   private
