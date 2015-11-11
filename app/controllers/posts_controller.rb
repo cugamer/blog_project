b7da7fb6c4f23fd68300edf4eaefa2907ba37ec2
Jonathon <jonathon.nordquist@gmail.com>
2015-11-11 22:18:22 +0000

    Create logged_in_user and correct_user methods in posts controller.  Set before filters to use these methods.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :logged_in_user, only: [:new, :create, :destroy, :edit, :update]
+  before_action :correct_user,   only: [:new, :create, :destroy, :edit, :update]
+  
   def index
   end
   
@@ -52,5 +55,18 @@ class PostsController < ApplicationController
   private
     def post_params
       params.require(:post).permit(:title, :body)
+    end
+    
+    def logged_in_user
+      unless logged_in?
+        flash[:errors] = ["Please log in to access this function"]
+        redirect_to login_url
+      end
+    end
+    
+    def correct_user
+      post = Post.find(params[:id])
+      @user = User.find(post.user_id)
+      redirect_to(root_url) unless @user == current_user
     end
 end
