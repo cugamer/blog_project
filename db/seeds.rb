be9053bb030c253356793cf76c9f22459fb89160
Jonathon <jonathon.nordquist@gmail.com>
2015-11-11 17:42:57 +0000

    Rework display of faker input.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-test_user = User.create!(user_name:                "snow",
+test_user = User.create!(user_name:               "snow",
                         name:                     "John Smith",
                         email:                    "john@smith.com",
                         password:                 "password",
@@ -6,8 +6,8 @@ test_user = User.create!(user_name:                "snow",
 
 50.times do
     prng = Random.new
-    title = Faker::Lorem.words(prng.rand(10))
-    body = Faker::Lorem.paragraphs(prng.rand(10))
+    title = Faker::Lorem.words(1..10).join(" ")
+    body = Faker::Lorem.paragraphs(prng.rand(1..100)).join("\n")
     test_user.posts.create(title: title,
                       body: body)
   end
@@ -24,8 +24,8 @@ test_user = User.create!(user_name:                "snow",
                      password_confirmation:  password)
   50.times do
     prng = Random.new
-    title = Faker::Lorem.words(prng.rand(10))
-    body = Faker::Lorem.paragraphs(prng.rand(10))
+    title = Faker::Lorem.words.join(" ")
+    body = Faker::Lorem.paragraphs(prng.rand(1..200)).join("\n")
     user.posts.create(title: title,
                       body: body)
   end
