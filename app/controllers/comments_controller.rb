a76c6c469d660b81c9d0f7ff0c614e5d0f850079
Jonathon <jonathon.nordquist@gmail.com>
2015-11-12 22:42:01 +0000

    Update comment form, comments create and comment_params methods to allow comments to be saved.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,15 +2,18 @@ class CommentsController < ApplicationController
   def create
     comment = Comment.new(comment_params)
     comment.user_id = current_user.id
+    p "========================================"
+    p comment_params
     if comment.save
       
     else
-      # redirect_to post_path(@post)
     end
+    
+    redirect_to post_path(comment_params[:post_id])
   end
   
   private
     def comment_params
-      params.require(:comments).permit(:comment_text)
+      params.require(:comment).permit(:comment_text, :post_id)
     end
 end
