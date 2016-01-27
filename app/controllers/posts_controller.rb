f478893fa2c0518431a5cf22c18ae55cb5e1f05e
Jonathon <jonathon.nordquist@gmail.com>
2016-01-27 17:47:31 +0000

    Removed new and create from before_action :correct_user filter in PostsController.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :logged_in_user, only: [:new, :create, :destroy, :edit, :update]
-  before_action :correct_user,   only: [:new, :create, :destroy, :edit, :update]
+  before_action :correct_user,   only: [:destroy, :edit, :update]
   
   def index
   end
